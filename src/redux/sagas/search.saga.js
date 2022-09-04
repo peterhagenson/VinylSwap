1a93e1015bce1923b37fc00f325ef807b90f81e6
Peter Hagenson <p.hagenson@gmail.com>
2022-09-04 08:43:45 -0500

    established route to server

--- src/redux/sagas/search.saga.js
+++ src/redux/sagas/search.saga.js
@@ -3,6 +3,13 @@ import axios from 'axios';
 
 function* getMatches(action) {
     console.log('in getMatches', action.payload)
+    try{
+        const matches = yield axios.get(`/searchDB/${action.payload}`);
+        // console.log('get results', matches.data)
+        yield put({type: 'SHOW_RESULTS', payload: matches.data})
+    } catch {
+        console.log('GET MATCHES ERROR');
+    }
     
 
 };
