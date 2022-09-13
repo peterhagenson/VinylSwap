3bf7b79b23b35cf00a7dbcecd17dc09a3494b1c3
Peter Hagenson <p.hagenson@gmail.com>
2022-09-13 06:34:19 -0500

    displaying message on DOM

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 
 
-function TemplateFunction(props) {
+function Messages(props) {
 
   const dispatch = useDispatch();
 
   const messages = useSelector((store) => store.messages)
+  // const [newMessage, setNewMessage] = useEffect('');
 
 
   const getMessages = () => {
@@ -20,18 +23,29 @@ function TemplateFunction(props) {
     getMessages();
   }, []);
 
+  // const sendMessage = () => {
+
+  // }
+
 
   return (
     <div>
-      {/* <h2>{heading}</h2> */}
       {JSON.stringify(messages)}
-      {/* {messages.map((thread) => {
-        if (thread) { */}
+      {messages.map((message) => {
+        return (
+          <p>{message.message}</p>
+        )
+      })}
 
-      {/* }
-      })} */}
+      {/* <form onSubmit={() => sendMessage()}>
+        <TextField onChange={(event) => (setNewMessage(event.target.value))} variant="outlined"
+          size="small" style={{ width: 300 }} sx={{ backgroundColor: 'white' }} placeholder="artist name or album title" value={newMessage} />
+        <br />
+        <Button variant="contained" sx={{ color: 'white', backgroundColor: 'black', mt: 2, mr: 1 }} type="submit" >Find</Button>
+      </form> */}
+
     </div>
   );
 }
 
-export default TemplateFunction;
+export default Messages;
